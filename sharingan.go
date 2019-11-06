c3a1bb71f2ace943ecab90871a33ae6767ec50a2
Ryan Wise <ryan@ryanwise.me>
2019-11-06 01:19:16 -0500

    Basic nmap scanning

--- sharingan.go
+++ sharingan.go
@@ -1,21 +1,24 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/Ullaakut/nmap"
 	"github.com/urfave/cli"
 )
 
 func main() {
 	sharingan := cli.NewApp()
-
 	sharingan.Name = "Sharingan"
 	sharingan.Usage = "Wrapper and analyzer for offensive security recon tools"
 
 	sharingan.Action = func(c *cli.Context) error {
-		fmt.Printf("Hello %q", c.Args().Get(0))
+		target := c.Args().Get(0)
+		ScanTarget(target)
 		return nil
 	}
 
@@ -24,4 +27,38 @@ func main() {
 		log.Fatal(err)
 	}
 
+}
+
+func ScanTarget(target string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	scanner, err := nmap.NewScanner(
+		nmap.WithTargets(target),
+		nmap.WithContext(ctx),
+	)
+
+	if err != nil {
+		log.Fatalf("Unable to create nmap scanner: %v", err)
+	}
+
+	result, err := scanner.Run()
+	if err != nil {
+		log.Fatalf("Unable to run nmap scan: %v", err)
+	}
+
+	// Use the results to print an example output
+	for _, host := range result.Hosts {
+		if len(host.Ports) == 0 || len(host.Addresses) == 0 {
+			continue
+		}
+
+		fmt.Printf("Host %q:\n", host.Addresses[0])
+
+		for _, port := range host.Ports {
+			fmt.Printf("\tPort %d/%s %s %s\n", port.ID, port.Protocol, port.State, port.Service.Name)
+		}
+	}
+
+	fmt.Printf("Nmap done: %d hosts up scanned in %3f seconds\n", len(result.Hosts), result.Stats.Finished.Elapsed)
 }
