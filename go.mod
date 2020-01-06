5da478e313386d0721a63d5534a15721b9b5152b
Ryan Wise <ryan@ryanwise.me>
2020-01-06 23:20:57 +0000

    Added nmap commands plus prompts, todo update readme

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/Ullaakut/nmap v2.0.0+incompatible
 	github.com/boltdb/bolt v1.3.1
+	github.com/manifoldco/promptui v0.6.0
 	github.com/objectbox/objectbox-go v1.1.0 // indirect
 	github.com/schollz/progressbar v1.0.0
 	github.com/schollz/progressbar/v2 v2.15.0
--- internal/app/cli/cli.go
+++ internal/app/cli/cli.go
@@ -1,15 +1,10 @@
 package cli
 
 import (
-	"fmt"
 	"log"
 	"os"
 
-	"text/tabwriter"
-
 	"github.com/leobeosab/sharingan/internal/models"
-	"github.com/leobeosab/sharingan/pkg/dns"
-	"github.com/leobeosab/sharingan/pkg/nmap"
 	"github.com/leobeosab/sharingan/pkg/storage"
 	"github.com/urfave/cli"
 )
@@ -50,8 +45,27 @@ func SetupCLI() {
 		},
 	}
 
+	sharingan.Commands = []cli.Command{
+		{
+			Name:  "dns",
+			Usage: "Perform a DNS scan",
+			Action: func(c *cli.Context) error {
+				RunDNSRecon(settings)
+				return nil
+			},
+		},
+		{
+			Name:  "scan",
+			Usage: "Perform a service scan using nmap -sV",
+			Action: func(c *cli.Context) error {
+				RunNmapScan(settings)
+				return nil
+			},
+		},
+	}
+
 	sharingan.Action = func(c *cli.Context) error {
-		RunDNSRecon(settings)
+		// RunDNSRecon(settings)
 		return nil
 	}
 
@@ -59,52 +73,4 @@ func SetupCLI() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func RunDNSRecon(settings *models.ScanSettings) {
-	if settings.Target == "" {
-		log.Fatal("Target needs to be defined")
-	}
-
-	if settings.DNSWordlist == "" {
-		log.Fatal("DNS Wordlist needs to be defined")
-	}
-
-	var s models.ScanResults
-
-	r := storage.RetrieveScanResults(settings.Store, settings.Target)
-	if len(r) == 0 || settings.Rescan {
-		s = models.ScanResults{
-			RootDomain: settings.Target,
-		}
-
-		s.Hosts = dns.DNSBruteForce(settings.Target, settings.DNSWordlist)
-
-		if !settings.SkipProbe {
-			nmap.FilterHosts(&s.Hosts)
-		}
-
-		if settings.Rescan {
-			storage.UpdateScan(settings.Store, &s)
-		} else {
-			storage.SaveScan(settings.Store, &s)
-		}
-	} else {
-		s = r[0]
-		fmt.Println("Found previous scan")
-	}
-
-	// Get a few lines of space before final result
-	fmt.Printf("\n\n")
-	// Pretty print the results
-	w := new(tabwriter.Writer)
-	w.Init(os.Stdout, 8, 8, 0, '\t', 0)
-
-	fmt.Fprintf(w, "\n%s\t%s\t", "Host Address ", "| Subdomain List")
-	fmt.Fprintf(w, "\n%s\t%s\t", "----------------- ", "| -------")
-	for _, h := range s.Hosts {
-		fmt.Fprintf(w, "\n%s \t| %v\t", h.IP, h.Subdomains)
-	}
-
-	w.Flush()
 }
